ed879e9951d8c068f25e5151ec0b6fdd2171c1f1
iOgre <igorvadimovich@outlook.com>
2018-04-30 02:56:23 +0500

    added style

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import CommentList from "./CommentList";
+
 class Article extends Component {
   
   state = {
@@ -13,15 +14,21 @@ class Article extends Component {
   }
   render() {
     const {article} = this.props
-    const body = this.state.isOpen && <section>{article.text}</section>
+    const body = this.state.isOpen && <section className="card-text">{article.text}</section>
     return (
-      <div>
+      <div className="card mx-auto" style={{width: '70%'}}>
+        <div className="card-header">
         <h2>{article.title}
-          <button onClick={this.handleClick}>{this.state.isOpen ? "close" : "open"}</button>
+          <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>{this.state.isOpen ? "close" : "open"}</button>
         </h2>
+        </div>
+        <div className="card-body">
+          <h6 className="card-subtitle text-muted">Creation date: {(new Date(article.date).toDateString())}</h6>
         {body}
-        <h3>Creation date: {(new Date(article.date).toDateString())}</h3>
+        </div>
+        <div style={{display: 'none' }} >
         <CommentList comments={article.comments}/>
+        </div>
       </div>
     )
   }
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,8 +3,10 @@ import Comment from './Comment'
 export default function CommentList({comments}) {
   const commentList = comments && comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)
   return (
+    <div className="container">
     <ul>
       {commentList}
     </ul>
+    </div>
   )
 }
